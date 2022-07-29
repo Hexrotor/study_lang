4b7cf08a072b6317f07fbdc49d7303f405d4796e
Hexrotor <tszbbb@qq.com>
2022-07-29 19:19:16 +0800

    Size of _Bool and long double

--- sizeof.c
+++ sizeof.c
@@ -16,6 +16,8 @@ int main()
     printf("The size of data 3.14 is %d\n", sizeof(pi));
     printf("The size of float is %d\n", sizeof(float));
     printf("The size of double is %d\n", sizeof(double));
+    printf("The size of long double is %d\n", sizeof(long double));
+    printf("The sizeof _Bool is %d\n", sizeof(_Bool));
 
     return 0;
 }
