e438c0fef18ccd8bcc79a56aeb26b5c0d875d277
Hexrotor <tszbbb@qq.com>
2022-09-17 10:14:28 +0800

    SESSIONID is temporary

--- shell/courseCheck.sh
+++ shell/courseCheck.sh
@@ -5,15 +5,15 @@ echo 访问选课页面需要设置Cookies，现在请按提示开始设置。
 echo 请先登录教务处，然后使用浏览器查看页面中的Cookies
 read -p "请输入GSESSIONID/JSESSIONID的值：" SESSIONID
 read -p "请输入semester.id的值：" semesterid
-read -r -p "是否要将当前输入值永久保存在脚本中？[Y/n] " save
+read -r -p "是否要将semester.id输入值永久保存在脚本中？[Y/n] " save
 #判断用户输入
 case $save in
 [yY][eE][sS]|[yY])
 #替换字符串
 #Linux路径，不适用于Windows
 thisPath=$(pwd)/$0
-sed -i "4,25d" ${thisPath}
-sed -i "s/\${SESSIONID}/${SESSIONID}/g ; s/\${semesterid}/${semesterid}/g" ${thisPath}
+sed -i "7,25d" ${thisPath}
+sed -i "s/\${semesterid}/${semesterid}/g" ${thisPath}
 ;;
 [nN][oO]|[nN])
 #do nothing
